33ffa3f1f8984d71465f6ae8590860288d237c6d
Nigel Norris <nigel@nigelnorris.com>
2024-06-10 09:12:40 -0400

    Update youcubeapi.lua

--- src/lib/youcubeapi.lua
+++ src/lib/youcubeapi.lua
@@ -32,9 +32,7 @@ end
 -- Look at the [Documentation](https://commandcracker.github.io/YouCube/) for moor information
 -- Contact the server owner on Discord, when the server is down
 local servers = {
-    "wss://mc.projectopencannibal.org", -- Your server!
-    "wss://youcube.knijn.one", -- By EmmaKnijn#0043
-    "wss://youcube.onrender.com", -- By Commandcracker#8528
+    "wss://mc.projectopencannibal.org"
 }
 
 if settings then
